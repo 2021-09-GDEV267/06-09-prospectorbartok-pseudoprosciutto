ca3fcb1098114238444de3efffdbb9e0f116c3f1
Matthew Sheehan <cementfactorypublishing@gmail.com>
2021-10-13 21:01:14 -0400

    added way to read the xml file for prospector

--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -28,6 +28,14 @@ public class Prospector : MonoBehaviour {
 	void Start() {
 		deck = GetComponent<Deck> (); //get the deck
 		deck.InitDeck (deckXML.text); // pass DeckXML to it
+		Deck.Shuffle(ref deck.cards);// thus shuffes deck by reference
+
+		Card c;
+		for (int cNum=0; cNum<deck.cards.Count;cNum++)
+        {
+			c = deck.cards[cNum];
+			c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
+        }
 	}
 
 }
