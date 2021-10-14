3158916197bb48298ac3e2da0522402b2d8548ce
Matthew Sheehan <cementfactorypublishing@gmail.com>
2021-10-13 22:11:55 -0400

    got the basic game working now just need to add scoring and my own game

--- Assets/Prospector/__Scripts/Card.cs
+++ Assets/Prospector/__Scripts/Card.cs
@@ -10,41 +10,103 @@ using System.Collections.Generic;
 /// described in the XML document—Figure 32.4 shows the differences between
 /// decorators and pips).
 /// </summary>
-public class Card : MonoBehaviour {
+public class Card : MonoBehaviour
+{
 
-	public string    suit;
-	public int       rank;
-	public Color     color = Color.black;
-	public string    colS = "Black";  // or "Red"
-	
+	public string suit;
+	public int rank;
+	public Color color = Color.black;
+	public string colS = "Black";  // or "Red"
+
 	public List<GameObject> decoGOs = new List<GameObject>();
 	public List<GameObject> pipGOs = new List<GameObject>();
-	
+
 	public GameObject back;  // back of card;
 	public CardDefinition def;  // from DeckXML.xml		
+    public SpriteRenderer[] spriteRenderers;
 
-
-	public bool faceUp {
-		get {
+	public bool faceUp
+	{
+		get
+		{
 			return (!back.activeSelf);
 		}
 
-		set {
+		set
+		{
 			back.SetActive(!value);
 		}
 	}
 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        SetSortOrder(0);  // Ensures that the card starts properly depth sorted
+    }
+
+    virtual public void OnMouseUpAsButton()
+    {
+        print(name); // When clicked, this outputs the card name
+    }
+
+
+    // If spriteRenderers is not yet defined, this function defines it
+    public void PopulateSpriteRenderers()
+    {
+        // If spriteRenderers is null or empty
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            // Get SpriteRenderer Components of this GameObject and its children
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+
+    // Sets the sortingLayerName on all SpriteRenderer Components
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    // Sets the sortingOrder of all SpriteRenderer Components
+
+    public void SetSortOrder(int sOrd)
+    {                                     
+        PopulateSpriteRenderers();
+        // Iterate through all the spriteRenderers as tSR
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                // If the gameObject is this.gameObject, it's the background
+                tSR.sortingOrder = sOrd; // Set it's order to sOrd
+                continue; // And continue to the next iteration of the loop
+            }
+            // Each of the children of this GameObject are named
+            // switch based on the names
+
+            switch (tSR.gameObject.name)
+            {
+                case "back": // if the name is "back"
+                    // Set it to the highest layer to cover the other sprites
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+
+                case "face":  // if the name is "face"
+                default:      //  or if it's anything else
+
+                    // Set it to the middle layer to be above the background          
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
 } // class Card
+
 
 [System.Serializable]
 public class Decorator{
@@ -59,5 +121,5 @@ public class CardDefinition{
 	public string	face;	//sprite to use for face cart
 	public int		rank;	// value from 1-13 (Ace-King)
 	public List<Decorator>	
-					pips = new List<Decorator>();  // Pips Used
+	pips = new List<Decorator>();  // Pips Used
 }
--- Assets/Prospector/__Scripts/CardProspector.cs
+++ Assets/Prospector/__Scripts/CardProspector.cs
@@ -25,4 +25,17 @@ public class CardProspector : Card
 
     // The SlotDef class stores information pulled in from the LayoutXML <slot>
     public SlotDef slotDef;
+
+    override public void OnMouseUpAsButton()
+    {
+
+        // Call the CardClicked method on the Prospector singleton
+
+        Prospector.S.CardClicked(this);
+
+        // Also call the base class (Card.cs) version of this method
+
+        base.OnMouseUpAsButton();                                                  // a
+
+    }
 }
